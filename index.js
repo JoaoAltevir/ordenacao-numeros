2cfccd1c134e2f43ad147ac88da2f9692b2c98ea
joaoaltevir <j.altevirneto@gmail.com>
2024-06-17 12:53:54 -0300

    feat: remove os numeros iguais, deixando apenas um

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let numeros = [4,12,4,5,6,7,1,9];
+let numeros = [4,12,4,5,6,7,1,9,1,1];
+
+for(let i = 0; i < numeros.length; i++){
+    for(let c = 1; c < numeros.length; c++){
+        if(numeros[i] == numeros[c]){
+            numeros.splice(c,1);
+        }
+    }
+}
