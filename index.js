dbe0c000d603cd5036024d317491c973e9fd2e76
Neto <j.altevirneto@gmail.com>
2024-06-18 08:49:39 -0300

    feat: apagando os numeros iguais

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-let numeros = [4,12,4,5,6,7,9,1,1];
-let maior = 0
+let numeros = [4, 12, 4, 5, 6, 7, 9, 1, 1];
+let maior = 0;
 let menor;
 let aux;
 
-for(let i = 0; i < numeros.length; i++){
-    for(let j = i + 1; j < numeros.length; j++){
-        if(numeros[i] > numeros[j]){
-            aux = numeros[i];
-            numeros[i] = numeros[j]
-            numeros[j] = aux;
-        }
+for (let i = 0; i < numeros.length; i++) {
+  for (let j = i + 1; j < numeros.length; j++) {
+    if (numeros[i] > numeros[j]) {
+      aux = numeros[i];
+      numeros[i] = numeros[j];
+      numeros[j] = aux;
     }
-}console.log(numeros)
+  }
+}
+console.log(numeros);
+for (let i = 0; i < numeros.length; i++) {
+  for (let j = i + 1; j < numeros.length; j++) {
+    if (numeros[i] == numeros[j]) {
+      numeros.splice(j,1);
+      j--;
+    }
+  }
+}
+console.log(numeros);
