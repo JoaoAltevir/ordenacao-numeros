97d0200332342fba5d4457947d996afc93a46b1a
Neto <j.altevirneto@gmail.com>
2024-06-18 08:40:33 -0300

    feat: array ordenado

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-let numeros = [4,12,4,5,6,7,1,9,1,1];
+let numeros = [4,12,4,5,6,7,9,1,1];
 let maior = 0
 let menor;
+let aux;
 
 for(let i = 0; i < numeros.length; i++){
-    for(let c = 1; c < numeros.length; c++){
-        if(numeros[i] == numeros[c]){
-            numeros.splice(c,1);
+    for(let j = i + 1; j < numeros.length; j++){
+        if(numeros[i] > numeros[j]){
+            aux = numeros[i];
+            numeros[i] = numeros[j]
+            numeros[j] = aux;
         }
     }
-}
-for(let i = 0; i < numeros.length; i++){
-    if(numeros[i] > maior){
-        maior = numeros[i];
-    }else if(numeros[i] < maior){
-        menor = numeros[i];
-    }else if(numeros[i] > menor && numeros[i] < maior){
-        numeros.splice(1,0,numeros[i])
-    }
-}
+}console.log(numeros)
